382cf159ddfe35034ac934a2f693b6b764a5b6b3
ShreyasChavan1 <152523720+ShreyasChavan1@users.noreply.github.com>
2025-10-27 19:41:18 +0530

    added worker service

--- backend/Fireebase/FirebaseAdmin.js
+++
@@ -1,9 +0,0 @@
-const admin = require("firebase-admin")
-const serviceAccount = require("./serviceAccountKey.json")
-
-admin.initializeApp({
-    credential:admin.credential.cert(serviceAccount),
-    storageBucket:"bhai-f58f8.appspot.com"
-})
-
-module.exports = admin;
